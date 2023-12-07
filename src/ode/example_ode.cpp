ce796ededa658d3df046d01c28b94ed702f26b6c
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2023-12-07 21:04:42 +0530

    Add files via upload

--- src/ode/example_ode.cpp
+++ src/ode/example_ode.cpp
@@ -1,26 +1,41 @@
 /*
 Using the odesolver library consists of:
-1. Defining your system dynamics derivative function f
-2. Constructing a Solver instance, passing f
+1. Defining your system dynamics derivative as a string
+2. Constructing a Solver instance, passing the string
 3. Calling Solve() to integrate through time
 
-Here is an example that solves the ODE dy/dt = 1
-from t = 0 to t = 10, with a timestep of 1.
+Here is an example that solves the ODE x'=-2*x+2 from t = 0
+to t = 10, with a timestep of 0.1. We plot the trajectory
+using a C++ interface for GNUPlot.
 */
 
 #include "ode_solver.hpp"
-#include <functional>
-
-std::vector<double> f(double t, std::vector<double> y)
-{
-  std::vector<double> dydt;
-  dydt.push_back(1.0);
-  return dydt;
-}
+#include "gnuplot.h"
+#include <fstream>
 
 int main()
 {
-  std::function<double(double, std::vector<double>)> func = f;
-  ode::Solver solver(func);
-  auto solution = solver.Solve(0.0, 10.0, 0.1, {1.0, 0.0});
+  // Solving the ODE
+  string ode = "x'=-2*x+2";
+  auto solver = ode::Solver(ode);
+  pair<double, double> initial;
+  initial.first = 0;
+  initial.second = 0;
+  auto trajectory = solver.Solve(100.0, 0.1, initial);
+
+  // Writing the trajectory into a data file
+  ofstream outfile;
+  outfile.open("trajectory.dat",ios::trunc);
+  for (pair<double, double> i : trajectory)
+  {
+    string str = to_string(i.first) + "  " + to_string(i.second) + "\n";
+    outfile << str;
+  }
+  outfile.close();
+
+  // Plotting using GNU
+  GnuplotPipe gp;
+  gp.sendLine("plot 'trajectory.dat' lt rgb 'red'");
+
+  return 0;
 }
