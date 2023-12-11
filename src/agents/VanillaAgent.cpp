3cb0261ceab843680c9d0b042d18a969a07b0b2a
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2023-12-11 16:11:05 +0530

    Delete src/agents/VanillaAgent.cpp

--- src/agents/VanillaAgent.cpp
+++
@@ -1,7 +0,0 @@
-/*
-The VanillaAgent class has a state (which is a collection of continuous C++ variables
-and a discrete mode, given by an enum Mode). It also consists of functions ContDynamics
-and DiscDynamics which define the continuous (flow and invariant of each mode)
-and the discrete (guard and assignments for each transition) dynamics of the system
-respectively.
-*/
