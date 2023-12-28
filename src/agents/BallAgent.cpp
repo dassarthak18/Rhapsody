c1dd1df8913f74d7d6e94bc2d6caf811573f6d1c
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2023-12-28 08:13:09 +0530

    Update BallAgent.cpp

--- src/agents/BallAgent.cpp
+++ src/agents/BallAgent.cpp
@@ -1,14 +1,16 @@
+#pragma once
+
 #include "VanillaAgent.hpp"
 
-class BallAgent
+class BallAgent : virtual public VanillaAgent
 {
   void DiscDynamics() override;
   
   public:
 
-    BallAgent(double x0, double v0, double horizon, double step)
+    BallAgent(double x0, double v0)
     {
-      pair<double, double> State;
+      pair<string, double> State;
 
       State.first = "x";
       State.second = x0;
@@ -20,15 +22,12 @@ class BallAgent
 
       ContDynamics.push_back("x'=v");
       ContDynamics.push_back("v'=-9.8");
-
-      time_horizon = horizon;
-      time_step = step;
     }
 };
 
 void BallAgent::DiscDynamics()
 {
-  if (ContVars[0] <= 0)
+  if (ContVars[0].second <= 0)
   {
     ContVars[0].second = 0;
     ContVars[1].second = -0.9*ContVars[1].second;
