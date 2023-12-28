6f1251b1689cadb222cf5c7d4612b031c6da516c
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2023-12-28 07:00:49 +0530

    Update VanillaAgent.hpp

--- src/agents/VanillaAgent.hpp
+++ src/agents/VanillaAgent.hpp
@@ -1,7 +1,38 @@
 /*
-The VanillaAgent class has a state (which is a collection of continuous C++ variables
-and a discrete mode, given by an enum Mode). It also consists of functions ContDynamics
-and DiscDynamics which define the continuous (flow and invariant of each mode)
-and the discrete (guard and assignments for each transition) dynamics of the system
-respectively.
+The simulate function follows the algorithm:
+  while t <= time_horizon:
+  {
+    initial x, v
+    replace ODE variables in RHS with current values
+    compute for one step and update x, v
+    run 1 pass of discrete dynamics
+  }
+  print the plot of the trajectory
 */
+
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+class VanillaAgent
+{ 
+  public:
+  	vector<pair<string, double>> ContVars;
+	vector<string> ContDynamics;
+	double time_horizon, time_step;
+	void Simulate();
+
+	void DiscDynamics()
+	{}
+
+    VanillaAgent(double horizon, double step)
+    {
+    	time_horizon = horizon;
+		time_step = step;
+    }
+};
+
+void VanillaAgent::Simulate()
+{
+	
+}
