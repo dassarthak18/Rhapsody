1df88eebefac7e1885fe805d4c619d06bb3df549
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-07-19 07:25:15 +0530

    Delete src/parser/parser.h

--- src/parser/parser.h
+++
@@ -1,7 +0,0 @@
-// parser.h
-#pragma once
-#include "json.hpp"
-#include <unordered_map>
-#include <string>
-
-std::unordered_map<std::string, nlohmann::json> parse_json_file(const std::string& filename);
